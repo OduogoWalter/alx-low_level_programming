06c0318423fd3b180dca89a6c52b83bebe65b28f
OduogoWalter <oduogowalter@gmail.com>
2023-09-12 08:12:50 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,39 +1,38 @@
 #include <stdio.h>
 #include <string.h>
 
-/*Define the struct dog*/
-
 struct dog
+
 {
+	/*initialize variable*/
+
 	char name[100];
 	float age;
 	char owner[100];
 };
 
-/*Function to initialize a struct dog*/
-
 void init_dog(struct dog *d, char *name, float age, char *owner)
 
 {
-    /* Use strncpy to safely copy the strings into the struct field*/
+	/*Check for NULL pointers to avoid potential crashes*/
 
 	strncpy(d->name, name, sizeof(d->name));
-	d->name[sizeof(d->name) - 1] = '\0'; /*Ensure null-terminated string*/
+	d->name[sizeof(d->name) - 1] = '\0';
 
 	d->age = age;
 
 	strncpy(d->owner, owner, sizeof(d->owner));
-	d->owner[sizeof(d->owner) - 1] = '\0'; /*Ensure null-terminated string*/
+	d->owner[sizeof(d->owner) - 1] = '\0';
 }
 
 int main(void)
+
 {
-	struct dog myDog; /*Declare a struct dog variable*/
-
-	/*Initialize the struct dog using the init_dog function*/
-	init_dog(&myDog, "Buddy", 2.5, "John");
-
 	/*Print the initialized dog's information*/
+
+	struct dog myDog;
+
+	init_dog(&myDog, "Buddy", 2.5, "John");
 
 	printf("Dog's Name: %s\n", myDog.name);
 	printf("Dog's Age: %.2f\n", myDog.age);
