7eb2d27507245023a87cc4299454e51df69105f1
OduogoWalter <oduogowalter@gmail.com>
2023-09-12 04:52:36 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/*
+ *main- Define a new type struct dog
+ *
+ * Return: Always 0 (success)
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
