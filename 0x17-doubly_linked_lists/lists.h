cce470b96b29f0673cdb7cb519831c331841a1f7
OduogoWalter <oduogowalter@gmail.com>
2023-11-25 00:52:16 -0800

    lists.h

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -13,6 +13,8 @@
  *
  * Description: doubly linked list node structure
  */
+
+/* Struct for doubly linked list nodes */
 typedef struct dlistint_s
 {
 	int n;
@@ -21,6 +23,7 @@ typedef struct dlistint_s
 }
 dlistint_t;
 
+/* Function prototypes */
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
@@ -30,5 +33,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+dlistint_t *insert_at_head(dlistint_t **h, dlistint_t *new_node);
+dlistint_t *link_at_index(dlistint_t *temp, dlistint_t *new_node);
 
 #endif /* LISTS_H */
